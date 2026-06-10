ead35f7f9a944dd43e86cfb7f8f3203147d6cf5a
sminbem <sminbem@naver.com>
2026-06-10 20:51:12 +0900

    데이터 타입 및 if 기초

--- 기본파이썬공부.py
+++ 기본파이썬공부.py
@@ -7,3 +7,29 @@ count = 100
 print(num + count, 문자변수)
 
 list = ['사과','배']
+# 데이터타입 확인
+print(type(list))
+#사칙연산자 *
+print("Hi")
+a = True
+b = False
+print(type(a))
+#삼항식
+age = 20
+print("성인" if age >= 19 else "미성년자")
+
+num = 1
+if num > 5:
+    print("5보다 크다")
+    print("나는 if식에 귀속되어있음")
+print("나는 위의 if 식과 상관없음")
+
+score = 75
+if score >= 90:
+    print(score,"점이라서 A군요")
+elif score >= 80:
+    print(score,"점이라서 B군요")
+elif score >= 70:
+    print(score,"점이라서 C군요")
+else:
+    print(score,"점이라서 F군요")       
